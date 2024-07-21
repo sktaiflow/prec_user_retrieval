d34a88e45e60b77043dd6a2459bdf500979fa37f
samaeui12 <smart0924@gmail.com>
2024-07-21 15:25:32 +0900

    add default airflow variables -> then merged

--- plugins/macros/airflow_variables_templates.py
+++ plugins/macros/airflow_variables_templates.py
@@ -2,18 +2,35 @@ from enum import Enum
 from airflow.models.variable import Variable
 
 
+DEFAULT_VARIABLES = {
+    "ENV": "stg",
+    "GCP_PROJECT_ID": "skt-datahub",
+    "DEBUG": "False",
+    "MAX_RETRIES": "3",
+    # Add more default variables as needed
+}
+
+
+def merge_variables(airflow_vars, default_vars):
+    """Merge Airflow variables with default variables."""
+    merged = default_vars.copy()
+    merged.update(airflow_vars)
+    return merged
+
+
 def fetch_all_variables():
     """Fetch all registered Airflow variables."""
     return Variable.get_all()
+
+
+def create_airflow_variables_enum():
+    """Create an Enum from merged Airflow and default variables. [priority: Airflow > Default variables]"""
+    airflow_vars = fetch_all_variables()
+    merged_vars = merge_variables(airflow_vars, DEFAULT_VARIABLES)
+    return Enum("AirflowVariables", merged_vars)
 
 
 class AirflowVariables(Enum):
     """Enum class to store all Airflow variables."""
 
     pass
-
-
-def create_airflow_variables_enum():
-    """Create an Enum from all Airflow variables."""
-    variables = fetch_all_variables()
-    return Enum("AirflowVariables", variables)
