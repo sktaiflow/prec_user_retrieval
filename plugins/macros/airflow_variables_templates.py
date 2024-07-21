385b2f00d925c6b772a3199537e0d1623805bfbc
samaeui12 <smart0924@gmail.com>
2024-07-21 17:27:23 +0900

    test

--- plugins/macros/airflow_variables_templates.py
+++ plugins/macros/airflow_variables_templates.py
@@ -1,7 +1,25 @@
 from airflow.models.variable import Variable
-
-from .utils.enum import StrEnum
+from enum import Enum
 from typing import Dict
+
+
+class StrEnum(str, Enum):
+    def _generate_next_value_(self, start, count, last_values):
+        return self
+
+    def __repr__(self) -> str:
+        return self.value
+
+    def __str__(self) -> str:
+        return self.value
+
+    @classmethod
+    def list_values(cls) -> list[str]:
+        return [e.value for e in cls]
+
+    @classmethod
+    def list_names(cls) -> list[str]:
+        return [e.name for e in cls]
 
 
 class DefaultVariablesEnum(StrEnum):
